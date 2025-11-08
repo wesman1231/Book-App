c4e5b64c0c47c92c1477eac9e5951d35e512456b
Wesley <wesquirk@gmail.com>
2025-11-07 18:40:03 -0800

    more helmet tweaking to allow images

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.use(helmet({
     useDefaults: true,
     directives: {
       "connect-src": ["'self'", "https://openlibrary.org"],
-      "img-src": ["'self'", "data:", "https://images.vexels.com"]
+      "img-src": ["'self'", "data:", "https://images.vexels.com", "https://covers.openlibrary.org", "https://static.vecteezy.com"]
     },
   },
 }));
